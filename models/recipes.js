79f350cf98c0ac8e88f25c10f00f4f0849fe2d5a
Dameon1 <dameonmedoza@gmail.com>
2018-09-11 15:45:52 -0700

    added models

---
+++ models/recipes.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+const recipeSchema = mongoose.Schema({
+  recipeId: Number,
+  userId: { type: mongoose.Schema.Types.ObjectId, 
+    ref: 'User', required: true }
+}, { timestamps: true});
+
+
+recipeSchema.set('toObject', {
+  transform: function (doc, ret) {
+    ret.id = ret._id;
+    delete ret._id;
+    delete ret.__v;
+  }
+});
+
+module.exports = mongoose.model('Recipe', recipeSchema);
+
