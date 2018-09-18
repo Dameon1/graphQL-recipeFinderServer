ab7e1ac997ea36bd6d948a51a61276953543fd2e
Dameon1 <dameonmedoza@gmail.com>
2018-09-17 22:18:20 -0700

    cleaned up index

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 'use strict';
 require('dotenv').config();
 const { ApolloServer, gql } = require('apollo-server');
-//const bodyParser = require('body-parser');
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
 const express = require('express');
@@ -10,22 +9,14 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const app = express();
-//const {router: recipeRouter} = require('./routes/recipes');
 const { PORT, CLIENT_ORIGIN ,MONGODB_URI} = require('./config');
-//const {router: usersRouter} = require('./routes/users');
-//const {router: authRouter} = require('./routes/auth');
 const jwtStrategy = require('./passport/jwt');
+
 passport.use(jwtStrategy);
 
 app.use(express.json());
 app.use(morgan(process.env.NODE_ENV === 'production' ? 'common' : 'dev', { skip: (req, res) => process.env.NODE_ENV === 'test' }));
 app.use(cors({ origin: CLIENT_ORIGIN }));
-
-
-// app.use('/api/users', usersRouter);
-// app.use('/api/login', authRouter);
-// app.use('/api/recipes', recipeRouter);
-
 
 if (require.main === module) {
   mongoose.connect(MONGODB_URI)
@@ -46,17 +37,6 @@ if (require.main === module) {
   });
 }
 
-// mongoose.connect('mongodb://localhost/test');
-
-// const Cat = mongoose.model('Cat', { name: String });
-
-// const PORT = 3000;
-
-// const app = express();
-
-// app.use('/graphql', bodyParser.json(), graphqlExpress({ schema, context: { Cat } }));
-
-// app.use('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
 
 const server = new ApolloServer({
   typeDefs,
